af89c7f13b682c261b89b556de132fc2d40eaeb8
Oliver Rein <o.rein@glb.de>
2024-10-19 10:29:13 +0200

    button B does not work yet

--- python/stadia-controller-setup.py
+++ python/stadia-controller-setup.py
@@ -59,81 +59,97 @@ for event in controller.read_loop():
 
     # Check for buttons for mouse actions
     elif event.type == ecodes.EV_KEY:
+        # A Button - like left click on mouse
         if event.code == ecodes.BTN_SOUTH and event.value == 1:  # A button pressed
             print("A button pressed -> Left click")
             device.emit(uinput.BTN_LEFT, 1)
             device.syn()  # Synchronize the input
         elif event.code == ecodes.BTN_SOUTH and event.value == 0:  # A button released
-            print("A button released -> Release left click")
             device.emit(uinput.BTN_LEFT, 0)
             device.syn()  # Synchronize the input
 
-        if event.code == ecodes.BTN_EAST and event.value == 1:  # B button pressed
-            print("B button pressed -> Right click")
-            device.emit(uinput.BTN_RIGHT, 1)
-            device.syn()  # Synchronize the input
-        elif event.code == ecodes.BTN_EAST and event.value == 0:  # B button released
-            print("B button released -> Release right click")
-            device.emit(uinput.BTN_RIGHT, 0)
-            device.syn()  # Synchronize the input
+    # # B Button 
+    # if event.code == ecodes.BTN_EAST and event.value == 1:  # B button pressed
+    #     print("B button pressed -> ALT + LEFT")
 
-        # Open Chromium when the START button (BTN_START) is pressed
-        if event.code == ecodes.BTN_START and event.value == 1:
-            print("Start button pressed -> Opening Chromium")
-            subprocess.Popen(["chromium-browser"])  # Launch Chromium
+    #     device.emit(uinput.KEY_LEFTALT, 1)   # ALT drücken
+    #     device.emit(uinput.KEY_LEFT, 1)      # LEFT drücken
+    #     device.syn()                         # Synchronisation
+    #     time.sleep(0.1)                      # Längere Verzögerung
 
-        # Simulate ALT + TAB + TAB when BTN_TR is pressed
-        if event.code == ecodes.BTN_TR and event.value == 1:
-            print("BTN_TR pressed -> Switching window")
-            # Simulate ALT + TAB + TAB using uinput
-            device.emit(uinput.KEY_LEFTALT, 1)  # Press ALT
-            device.emit(uinput.KEY_TAB, 1)      # Press TAB
-            time.sleep(0.05)                    # Small delay
-            device.syn()                        # Synchronize the input
-            device.emit(uinput.KEY_TAB, 0)      # Release TAB
-            device.emit(uinput.KEY_LEFTALT, 0)  # Release ALT
-            device.syn()                        # Synchronize the input
-
-        # Simulate ALT + SHIFT + TAB for backward (left) window switching
-        if event.code == ecodes.BTN_TL and event.value == 1:  # Example button for backward switching
-            print("BTN_TL pressed -> Switching to the previous window")
-            device.emit(uinput.KEY_LEFTALT, 1)   # Press ALT
-            device.emit(uinput.KEY_LEFTSHIFT, 1) # Press SHIFT
-            device.emit(uinput.KEY_LEFTALT, 1)   # Hold ALT
-            time.sleep(0.05)                     # Small delay
-
-            device.emit(uinput.KEY_TAB, 1)       # Press TAB
-            device.emit(uinput.KEY_TAB, 0)       # Release TAB
-            device.syn()                         # Synchronize the input
-
-            device.emit(uinput.KEY_LEFTSHIFT, 0) # Release SHIFT
-            device.emit(uinput.KEY_LEFTALT, 0)   # Release ALT
-            device.emit(uinput.KEY_LEFTALT, 0)   # Release ALT
-            device.syn()                         # Synchronize the input
+    #     device.emit(uinput.KEY_LEFTALT, 0)   # ALT loslassen
+    #     device.emit(uinput.KEY_LEFT, 0)      # LEFT loslassen
+    #     device.syn()                         # Synchronisation
+    #     time.sleep(0.1)                      # Längere Verzögerung
 
 
-    	# Simulate ALT + F4 when BTN_NORTH is pressed
-        # if event.code == ecodes.BTN_NORTH and event.value == 1:
-        #     print("BTN_NORTH pressed -> Closing the current window")
-        #     # Simulate pressing ALT
-        #     device.emit(uinput.KEY_LEFTALT, 1)  # Press ALT
+    # Y Button - like right click on mouse
+    if event.code == ecodes.BTN_WEST and event.value == 1:  # Y pressed
+        print("Y button pressed -> Right click")
+        device.emit(uinput.BTN_RIGHT, 1)
+        device.syn()  # Synchronize the input
+    elif event.code == ecodes.BTN_WEST and event.value == 0:  # Y released
+        device.emit(uinput.BTN_RIGHT, 0)
+        device.syn()  # Synchronize the input
 
-        #     # Simulate pressing F4
-        #     device.emit(uinput.KEY_F4, 1)       # Press F4
+    # Burger menue - Open Chromium when the START button (BTN_START) is pressed
+    if event.code == ecodes.BTN_START and event.value == 1:
+        print("Burger menue button pressed -> Opening Chromium")
+        subprocess.Popen(["chromium-browser"])  # Launch Chromium
 
-        #     # Simulate releasing F4
-        #     device.emit(uinput.KEY_F4, 0)       # Release F4
+    # R1 - Simulate ALT + TAB + TAB when BTN_TR is pressed
+    if event.code == ecodes.BTN_TR and event.value == 1:
+        print("R1 button pressed -> Switching window")
+        # Simulate ALT + TAB + TAB using uinput
+        device.emit(uinput.KEY_LEFTALT, 1)  # Press ALT
+        device.emit(uinput.KEY_TAB, 1)      # Press TAB
+        time.sleep(0.05)                    # Small delay
+        device.syn()                        # Synchronize the input
+        device.emit(uinput.KEY_TAB, 0)      # Release TAB
+        device.emit(uinput.KEY_LEFTALT, 0)  # Release ALT
+        device.syn()                        # Synchronize the input
 
-        #     # Simulate releasing ALT
-        #     device.emit(uinput.KEY_LEFTALT, 0)  # Release ALT
-        #     device.syn()                        # Synchronize the input
+    # L1 - Simulate ALT + SHIFT + TAB for backward (left) window switching
+    if event.code == ecodes.BTN_TL and event.value == 1:  # Example button for backward switching
+        print("L1 button pressed -> Switching to the previous window")
+        device.emit(uinput.KEY_LEFTALT, 1)   # Press ALT
+        device.emit(uinput.KEY_LEFTSHIFT, 1) # Press SHIFT
+        device.emit(uinput.KEY_LEFTALT, 1)   # Hold ALT
+        time.sleep(0.05)                     # Small delay
 
-        # Stop the script when the Assitant button (BTN_TRIGGER_HAPPY1) is pressed
-        if event.code == ecodes.BTN_TRIGGER_HAPPY1 and event.value == 1:
-            print("Assitant button pressed -> Exiting")
-            break
+        device.emit(uinput.KEY_TAB, 1)       # Press TAB
+        device.emit(uinput.KEY_TAB, 0)       # Release TAB
+        device.syn()                         # Synchronize the input
 
-        # Exit with the ESC key for a clean termination
-        if event.code == ecodes.KEY_ESC and event.value == 1:
-            print("ESC key pressed -> Exiting")
-            break
+        device.emit(uinput.KEY_LEFTSHIFT, 0) # Release SHIFT
+        device.emit(uinput.KEY_LEFTALT, 0)   # Release ALT
+        device.emit(uinput.KEY_LEFTALT, 0)   # Release ALT
+        device.syn()                         # Synchronize the input
+
+
+    # Simulate ALT + F4 when BTN_NORTH is pressed
+    # # X
+    # if event.code == ecodes.BTN_NORTH and event.value == 1:
+    #     print("BTN_NORTH pressed -> Closing the current window")
+    #     # Simulate pressing ALT
+    #     device.emit(uinput.KEY_LEFTALT, 1)  # Press ALT
+
+    #     # Simulate pressing F4
+    #     device.emit(uinput.KEY_F4, 1)       # Press F4
+
+    #     # Simulate releasing F4
+    #     device.emit(uinput.KEY_F4, 0)       # Release F4
+
+    #     # Simulate releasing ALT
+    #     device.emit(uinput.KEY_LEFTALT, 0)  # Release ALT
+    #     device.syn()                        # Synchronize the input
+
+    # Assitant - Stop the script when the Assitant button (BTN_TRIGGER_HAPPY1) is pressed
+    if event.code == ecodes.BTN_TRIGGER_HAPPY1 and event.value == 1:
+        print("Assitant button pressed -> Exiting")
+        break
+
+    # Keyboard - ESC - Exit with the ESC key for a clean termination
+    if event.code == ecodes.KEY_ESC and event.value == 1:
+        print("ESC key pressed -> Exiting")
+        break
